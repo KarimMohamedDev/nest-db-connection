d682ce144189e68291fc9ed31b6f68e9a19094d8
KarimMohamed6 <km6777804@gmail.com>
2024-12-29 18:44:02 +0200

    feat: add database module and refactor usery - Implemented DatabaseModule to configure TypeORM with PostgreSQL. - Refactored User entity to align with database schema.

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,7 +1,19 @@
-export class UserEntity {
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   username: string;
+
+  @Column()
   email: string;
+
+  @Column()
   password: string;
+
+  @Column()
   country: string;
 }
